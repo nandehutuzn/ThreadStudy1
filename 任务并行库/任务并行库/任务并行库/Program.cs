a68a8f8446b417208091ac493ee239a2750e857c
nandehutuzn <120902259@qq.com>
2016-11-03 20:57:45 +0800

    并行

---
+++ 任务并行库/任务并行库/任务并行库/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace 任务并行库
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Task(() => TaskMethod("Task 1"));
+            var t2 = new Task(() => TaskMethod("Task 2"));
+            t2.Start();
+            t1.Start();
+            Task.Run(() => TaskMethod("Task 3"));
+            Task.Factory.StartNew(() => TaskMethod("Task 4"));
+            Task.Factory.StartNew(() => TaskMethod("Task 5"), TaskCreationOptions.LongRunning);  //非线程池线程
+            Console.ReadKey();
+        }
+
+        static void TaskMethod(string name)
+        {
+            Console.WriteLine("Task {0} is running id: {1}. Is thread pool thread: {2}",
+                name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+        }
+    }
+}
---
+++ 任务并行库/任务并行库/基本操作/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace 基本操作
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskMethod("Main Thread Task");
+            Task<int> task = CreateTask("Task 1");
+            task.Start();
+            int result = task.Result;
+            Console.WriteLine("Result is: {0}", result);
+
+            task = CreateTask("Task 2");
+            task.RunSynchronously();  //在主线程中运行
+            result = task.Result;
+            Console.WriteLine("Result is: {0}", result);
+
+            task = CreateTask("Task 3");
+            task.Start();
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            }
+
+            Console.WriteLine(task.Status);
+            result = task.Result;
+            Console.WriteLine("Result is: {0}", result);
+            Console.ReadKey();
+        }
+
+        static Task<int> CreateTask(string name)
+        {
+            return new Task<int>(() => TaskMethod(name));
+        }
+
+        static int TaskMethod(string name)
+        {
+            Console.WriteLine("Task {0} is running on a thread id {1}, is thread pool thread: {2}",
+                name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            return 42;
+        }
+    }
+}
