6e04f71f2aed5eefba9c0c2fa739c40c579cf896
nandehutuzn <120902259@qq.com>
2016-11-03 23:27:52 +0800

    APMToTPL

---
+++ 任务并行库/任务并行库/APMToTPL/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace APMToTPL
+{
+    class Program
+    {
+        private delegate string AsyncchronousTasks(string threadName);
+
+        private delegate string IncompatibleAsynchronousTask(out int threadID);
+
+        static void Main(string[] args)
+        {
+            int threadID;
+            AsyncchronousTasks d = Test;
+            IncompatibleAsynchronousTask e = Test;
+
+            Console.WriteLine("Option 1");
+            Task<string> task = Task<string>.Factory.FromAsync(
+                d.BeginInvoke("AsyncTaskThead", Callback, "a delegate asynchronous call"), d.EndInvoke);
+
+            task.ContinueWith(t => Console.WriteLine("Callback is finished,now running a continuation! Result: {0}", t.Result));
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(task.Status);
+            Thread.Sleep(1000);
+
+            Console.WriteLine("--------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Option 2");
+
+            task = Task<string>.Factory.FromAsync(
+                d.BeginInvoke, d.EndInvoke, "AsyncTaskThread", "A delegate asynchronous call");
+
+            task.ContinueWith(t => Console.WriteLine("Task is completed, now running a continuation! Result: {0}", t.Result));
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(task.Status);
+            Thread.Sleep(1000);
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Option 3");
+
+            IAsyncResult ar = e.BeginInvoke(out threadID, Callback, "a delegate asynchronous call");
+            task = Task<string>.Factory.FromAsync(ar, o => e.EndInvoke(out threadID, ar));
+
+            task.ContinueWith(t => Console.WriteLine("Task is completed, now running a contiuation! Result: {0}, ThreadID: {1}", t.Result, threadID));
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(task.Status);
+
+            Console.ReadKey();
+        }
+
+        private static void Callback(IAsyncResult ar)
+        {
+            Console.WriteLine("Starting a callback ...");
+            Console.WriteLine("State passed to a callback: {0}", ar.AsyncState);
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Console.WriteLine("Thread pool worker thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static string Test(string threadName)
+        {
+            Console.WriteLine("Start .....");
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(2000);
+            return string.Format("Thread name: {0}", threadName);
+        }
+
+        private static string Test(out int threadID)
+        {
+            Console.WriteLine("Starting ...");
+            Console.WriteLine("Is thread pool thread: {0}", Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(2000);
+            threadID = Thread.CurrentThread.ManagedThreadId;
+            return string.Format("Thread pool worker thread id was : {0}", threadID);
+        }
+    }
+}
---
+++ 任务并行库/任务并行库/组合任务/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace 组合任务
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstTask = new Task<int>(() => TaskMethod("First Task", 3));
+            var secondTask = new Task<int>(() => TaskMethod("Second Task", 2));
+
+            firstTask.ContinueWith(
+                t => Console.WriteLine("The first answer is {0}. Thread is{1}, is thread pool thread: {2}",
+                    t.Result, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread),
+                    TaskContinuationOptions.OnlyOnRanToCompletion);
+
+    
+            secondTask.Start();
+            firstTask.Start();
+           
+
+            Thread.Sleep(TimeSpan.FromSeconds(4));
+            secondTask.ContinueWith(
+       t =>
+       {
+           Console.WriteLine("The second answer is {0}. Thread is{1}, is thread pool thread: {2}",
+           t.Result, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+           int tt = t.Result;
+       },
+           TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously);//直接在主线程中执行,如果得到结果的话
+           
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine();
+
+            firstTask = new Task<int>(() =>
+            {
+                var innerTask = Task.Factory.StartNew(() => TaskMethod("Child Task", 5), TaskCreationOptions.AttachedToParent);
+                innerTask.ContinueWith(t => TaskMethod("Child Task Continue", 2), TaskContinuationOptions.AttachedToParent);
+                return TaskMethod("Parent Task", 2);
+            });
+            firstTask.Start();
+
+            while (!firstTask.IsCompleted)
+            {
+                Console.WriteLine(firstTask.Status);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(firstTask.Status);
+            Console.ReadKey();
+        }
+
+        static int TaskMethod(string name, int seconds)
+        {
+            Console.WriteLine("Task {0} is running on a thread id {1}, is thread pool: {2}",
+                name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            return 42 * seconds;
+        }
+    }
+}
